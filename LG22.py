f5dc69b3f1b5ac79505f6e98cd0a294ebbf9f49b
H3 <hacker3000@posteo.org>
2024-12-14 01:48:03 +0100

    decode tested as working

--- LG22.py
+++ LG22.py
@@ -1,64 +1,97 @@
+useage="LG22.py <encode,decode> <input_file> <output_file> (encoded_name)"
+
+
+from struct import pack, unpack
+from sys import argv as arguments
+from hashlib import md5
+
+
 #encode
-
-<?php
-$filename = "1000weisseLilien.g722";
-$data = file_get_contents("1000weisseLilien.g722");
-
-$key = md5($data, true);
-
-$output = "";
-$output .= "GRT1";
-$output .= pack("V", 4 + 4 + 1 + strlen($filename) + 16 + 4 + strlen($data));
-$output .= pack("C", strlen($filename));
-$output .= $filename;
-$output .= $key;
-$output .= pack("V", strlen($data));
-
-for ($i=0; $i < strlen($data); $i++)
-{
-	$output .= chr(ord($data[$i]) ^ ((ord($key[$i % 16]) + $i) & 0xFF));
-}
-
-file_put_contents("1000weisseLilien.L22", $output);
+def encode(decoded_filename,encoded_filename,trackname):
+    data=b""
+    with open(decoded_filename,"rb") as df:
+        data = df.read()
+    
+    m = md5(usedforsecurity=False)
+    m.update(data)
+    key = m.digest()
+    
+    with open(encoded_filename,"wb") as ef:
+        file.write("GRT1")
+        #little endian uint32_t followed by uint_8
+        file.write(pack("<LB", (4 + 4 + 1 + len(filename) + 16 + 4 + len(data),  len(filename)) ))
+        file.write(filename)
+        file.write(key)
+        file.write(pack("L", len(data)))
+        #write "encrypted" data
+        for i in range(0,len(data)):
+            file.write(pack("B", data[i] ^ ((key[i % 16] + i) & 0xFF)))
 
 
 #decode
+def decode(encoded_filename,decoded_filename):
+    data=b""
+    with open(encoded_filename,"rb") as ef:
+        data = ef.read()
+    
+    # skip header "GRT1"
+    data = data[4:]
+    
+    # extract length
+    length = unpack("L", data[:4])[0]
+    data = data[4:]
+    
+    # extract filename
+    filename_length = unpack("B", data[:1])[0]
+    data = data[1:]
+    
+    filename = data[:filename_length]
+    data = data[filename_length:]
+    
+    print("filename: " + filename.decode("ascii"))
+    
+    # extract key
+    key = data[:16]
+    data = data[16:]
+    
+    print("key: " + key.hex())
+    
+    # extract payload length
+    payload_length = unpack("L", data[:4])[0]
+    data = data[4:]
+    
+    # "decrypt" payload
+    with open(decoded_filename,"wb") as df:
+        for i in range(0,len(data)):
+            df.write(pack("B", data[i] ^ ((key[i % 16] + i) & 0xFF)))
+    
+    # ffmpeg -f g722 -i 1000weisseLilien.L22.decode.g722 -ar 22050 test.wav
 
-<?php
-$data = file_get_contents("1000weisseLilien.L22");
 
-// skip header "GRT1"
-$data = substr($data, 4);
+#argument handling
+if len(arguments) < 2:
+    quit(useage)
+mode = arguments[1]
+if mode == "encode":
+    if len(arguments) not in [4,5]:
+        print("invalid argument count. correct useage:")
+        print(useage)
+        quit()
+    mode==True
+elif mode == "decode":
+    if len(arguments) != 4:
+        print("invalid argument count. correct useage:")
+        print(useage)
+        quit()
+    mode=False
+else:
+    print("invalid arguments. correct useage:")
+    print(useage)
+    quit()
 
-// extract length
-$length = unpack("V", substr($data, 0, 4))[1];
-$data = substr($data, 4);
+if mode:
+    trackname=arguments[4] if len(arguments)==5 else arguments[3]
+    encode(arguments[2],arguments[3], trackname)
+else:
+    decode(arguments[2],arguments[3])
 
-// extract filename
-$filename_length = unpack("C", substr($data, 0, 1))[1];
-$data = substr($data, 1);
-
-$filename = substr($data, 0, $filename_length);
-$data = substr($data, $filename_length);
-
-error_log("filename: " . $filename);
-
-// extract key
-$key = substr($data, 0, 16);
-$data = substr($data, 16);
-
-error_log("key: " . bin2hex($key));
-
-// extract payload length
-$payload_length = unpack("V", substr($data, 0, 4))[1];
-$data = substr($data, 4);
-
-// "decrypt" payload
-$decode = "";
-for ($i=0; $i < $payload_length; $i++)
-{
-	$decode .= chr(ord($data[$i]) ^ ((ord($key[$i % 16]) + $i) & 0xFF));
-}
-
-file_put_contents("1000weisseLilien.L22.decode.g722", $decode);
-// ffmpeg -f g722 -i 1000weisseLilien.L22.decode.g722 -ar 22050 test.wav
